5311a4f120039c226f3b4b186ec4b0cd28ffeb5c
Vrekrer <diego.gonzalez.chavez@gmail.com>
2017-06-13 14:10:13 -0300

    experiments added

---
+++ magdynlab/experiments/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+# Author: Diego González Chávez
+# email : diegogch@cbpf.br / diego.gonzalez.chavez@gmail.com
+#
+# magdynlab.experiments package
+#
+# TODO:
+# Make documentation
+
+
